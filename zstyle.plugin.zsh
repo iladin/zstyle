a818f937374e8b95ff43bd68d8f00d24ebcc6593
iladin <iladin@gmail.com>
2016-07-28 16:02:49 -0700

    Added process completion

--- zstyle.plugin.zsh
+++ zstyle.plugin.zsh
@@ -7,3 +7,8 @@ zstyle ':completion::complete:*' cache-path "${ZDOTDIR:-$HOME}/.zcompcache"
 
 #style’s value will be the description for options not described by functions
 zstyle ':completion:*:options' auto-description '%d'
+
+# Refer to your own processes
+zstyle ':completion:*:*:*:*:processes' command 'ps -u $LOGNAME -o pid,user,command -w'
+# colorful kill completion
+zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;36=0=01'
