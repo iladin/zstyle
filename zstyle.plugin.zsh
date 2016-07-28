1908de01277c4bb2f371e9854a24c11c076c9a35
iladin <iladin@gmail.com>
2016-07-28 15:53:11 -0700

    Used cache

--- zstyle.plugin.zsh
+++ zstyle.plugin.zsh
@@ -1,2 +1,6 @@
 # Do menu-driven completion.
 zstyle ':completion:*' menu select
+
+# Use cache
+zstyle ':completion::complete:*' use-cache on
+zstyle ':completion::complete:*' cache-path "${ZDOTDIR:-$HOME}/.zcompcache"
