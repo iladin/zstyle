969514c50bb4fd1566125f7dabc605b84200b4ab
iladin <iladin@gmail.com>
2016-07-28 15:58:35 -0700

    Added auto-description

--- zstyle.plugin.zsh
+++ zstyle.plugin.zsh
@@ -4,3 +4,6 @@ zstyle ':completion:*' menu select
 # Use cache
 zstyle ':completion::complete:*' use-cache on
 zstyle ':completion::complete:*' cache-path "${ZDOTDIR:-$HOME}/.zcompcache"
+
+#style’s value will be the description for options not described by functions
+zstyle ':completion:*:options' auto-description '%d'
