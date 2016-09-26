e983c4b9c0157925d844f624b20bad3999926598
Daniel Silverman <iladin@gmail.com>
2016-09-25 20:38:26 -0700

    Removed autoload -Uz

--- zstyle.plugin.zsh
+++ zstyle.plugin.zsh
@@ -53,14 +53,3 @@ bindkey -M menuselect '^l' vi-forward-char
 compdef g='git'
 compdef cfg='git'
 
-autoload -Uz cdr
-autoload -Uz history-search-end
-autoload -Uz modify-current-argument
-autoload -Uz smart-insert-last-word
-autoload -Uz terminfo
-autoload -Uz vcs_info
-autoload -Uz zcalc
-autoload -Uz zmv
-autoload -Uz run-help-git
-autoload -Uz run-help-svk
-autoload -Uz run-help-svn
